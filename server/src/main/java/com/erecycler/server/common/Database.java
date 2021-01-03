ab55bcae2f450556cb86d0f2d2afdd02ac882320
amy choi <cgc8016@ewhain.net>
2021-01-03 22:57:42 +0900

    fix(Database): credential load from .json file -> export system env variable(GOOGLE_APPLICATION_CREDENTIAL)

--- server/src/main/java/com/erecycler/server/common/Database.java
+++ server/src/main/java/com/erecycler/server/common/Database.java
@@ -1,32 +1,25 @@
 package com.erecycler.server.common;
 
-import static com.erecycler.server.common.ErrorCase.DATABASE_CONNECTION_ERROR;
-
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
 import javax.annotation.PostConstruct;
 import org.springframework.stereotype.Service;
-import org.springframework.util.ResourceUtils;
 
 @Service
 public class Database {
 	@PostConstruct
 	public static void loadDatabase() {
 		try {
-			File file = ResourceUtils
-				.getFile("classpath:re-cycle-app-firebase-adminsdk-1gqoj-deef252d80.json");
-			InputStream serviceAccount = new FileInputStream(file);
-			GoogleCredentials credentials = GoogleCredentials.fromStream(serviceAccount);
+			// system environment GOOGLE_APPLICATION_CREDENTIAL setting ahead
+			GoogleCredentials credentials = GoogleCredentials.getApplicationDefault();
 			FirebaseOptions options = new FirebaseOptions.Builder()
 				.setCredentials(credentials)
+				.setProjectId("re-cycle-app")
 				.build();
 			FirebaseApp.initializeApp(options);
 		} catch (Exception e) {
-			System.out.println(DATABASE_CONNECTION_ERROR);
+			System.out.println(ErrorCase.DATABASE_CONNECTION_ERROR);
 			e.printStackTrace();
 		}
 	}
