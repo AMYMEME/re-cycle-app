ba3a222817a6fa62408fc98ee7f8fbe39f3f35b0
amy choi <cgc8016@ewhain.net>
2021-01-03 23:53:52 +0900

    feat(ErrorCase): add errorCase

--- server/src/main/java/com/erecycler/server/common/ErrorCase.java
+++ server/src/main/java/com/erecycler/server/common/ErrorCase.java
@@ -1,5 +1,6 @@
 package com.erecycler.server.common;
 
 public class ErrorCase {
-	public static final String DATABASE_CONNECTION_ERROR = "데이터베이스에 연결할 수 없습니다. ";
+	public static final String DATABASE_CONNECTION_ERROR = "데이터베이스 연결 에러";
+	public static final String EMPTY_GUIDELINE_ERROR = "분리수거 방법란(guideline) 공백 에러";
 }
