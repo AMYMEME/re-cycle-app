694d269d2c7dd352ac1d653a7f31a2860bb2608a
amy choi <cgc8016@ewhain.net>
2021-01-06 00:31:55 +0900

    feat(ErrorCase): add errorCase

--- server/src/main/java/com/erecycler/server/common/ErrorCase.java
+++ server/src/main/java/com/erecycler/server/common/ErrorCase.java
@@ -4,4 +4,5 @@ public class ErrorCase {
 	public static final String DATABASE_CONNECTION_ERROR = "데이터베이스 등 인터넷 연결에 문제가 있습니다. ";
 	public static final String EMPTY_GUIDELINE_ERROR = "분리수거 방법(guideline)을 입력해주세요. ";
 	public static final String NO_SUCH_MATERIAL_ERROR = "해당 재질은 존재하지 않습니다. ";
+	public static final String NO_SUCH_ITEM_ERROR = "해당 내용이 존재하지 않습니다. ";
 }
