4dcfd05595c9b9f7edf6915f113761c5c0b6b721
amy choi <cgc8016@ewhain.net>
2021-01-05 23:36:33 +0900

    feat(ErrorCase): fix error statement and add error case

--- server/src/main/java/com/erecycler/server/common/ErrorCase.java
+++ server/src/main/java/com/erecycler/server/common/ErrorCase.java
@@ -1,6 +1,7 @@
 package com.erecycler.server.common;
 
 public class ErrorCase {
-	public static final String DATABASE_CONNECTION_ERROR = "데이터베이스 연결 에러";
-	public static final String EMPTY_GUIDELINE_ERROR = "분리수거 방법란(guideline) 공백 에러";
+	public static final String DATABASE_CONNECTION_ERROR = "데이터베이스 등 인터넷 연결에 문제가 있습니다. ";
+	public static final String EMPTY_GUIDELINE_ERROR = "분리수거 방법(guideline)을 입력해주세요. ";
+	public static final String NO_SUCH_MATERIAL_ERROR = "해당 재질은 존재하지 않습니다. ";
 }
