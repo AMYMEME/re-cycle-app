98416dd6149beb9e6f46b9f4a9db92e22d46e6e4
amy choi <cgc8016@ewhain.net>
2021-02-19 00:38:00 +0900

    feat(guro_cloth)

--- data/main.py
+++ data/main.py
@@ -1,3 +1,5 @@
 from add_data_firestore.recycle_guide import add_csv_data
+from kakao_map_location.guro import get_csv_data
 
-add_csv_data("parsed_csv/recycle-guide.csv")
+# add_csv_data("parsed_csv/recycle-guide.csv")
+get_csv_data("raw_data/cloth/csv/서울특별시 구로구_의류수거함_20200930.csv", "parsed_csv/cloth/guro.csv", "parsed_csv/cloth/missed.csv")
