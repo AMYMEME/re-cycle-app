f0bbdba5267e86f49f322573bc7d9a4ed6d4b277
amy choi <cgc8016@ewhain.net>
2021-02-19 00:59:57 +0900

    feat(seodaemun_cloth)

--- data/main.py
+++ data/main.py
@@ -1,7 +1,6 @@
 from add_data_firestore.recycle_guide import add_csv_data
-from kakao_map_location.guro import get_cloth_csv_data
-from kakao_map_location.dongjak import get_cloth_csv_data
+from kakao_map_location import soedaemun
 
 # add_csv_data("parsed_csv/recycle-guide.csv")
 # get_cloth_csv_data("raw_data/cloth/csv/서울특별시 구로구_의류수거함_20200930.csv", "parsed_csv/cloth/guro.csv", "parsed_csv/cloth/missed.csv")
-get_cloth_csv_data("raw_data/cloth/csv/서울특별시 동작구_의류수거함 위치 데이터_20200928.csv", "parsed_csv/cloth/dongjak.csv", "parsed_csv/cloth/missed.csv")
+soedaemun.get_cloth_csv_data("raw_data/cloth/csv/서울특별시 서대문구_서대문구 의류수거함 위치현황_20201005.csv", "parsed_csv/cloth/soedaemun.csv", "parsed_csv/cloth/missed.csv")
