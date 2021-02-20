cf2d7de64dc68fb529b369cbbf0f6e72cb9a6e2f
amy choi <cgc8016@ewhain.net>
2021-02-21 00:20:39 +0900

    feat(songpa_lamp_battery)

--- data/main.py
+++ data/main.py
@@ -1,7 +1,7 @@
 from add_data_firestore.recycle_guide import add_csv_data
-from kakao_map_location import sungbuk
+from kakao_map_location import songpa
 
 # add_csv_data("parsed_csv/recycle-guide.csv")
 # get_cloth_csv_data("raw_data/cloth/csv/서울특별시 구로구_의류수거함_20200930.csv", "parsed_csv/cloth/guro.csv", "parsed_csv/cloth/missed.csv")
 # dongjak.get_lamp_battery_csv_data("raw_data/lamp_battery/csv/서울특별시 동작구 폐형광등(폐건전지) 분리수거함 현황_20200820.csv", "parsed_csv/lamp_battery/dongjak.csv", "parsed_csv/lamp_battery/missed.csv")
-sungbuk.get_lamp_battery_csv_data("raw_data/lamp_battery/csv/서울특별시_성북구_폐형광등폐건전지수거함_20190823.csv", "parsed_csv/lamp_battery/sungbuk.csv")#, "parsed_csv/lamp_battery/missed.csv")
+songpa.get_lamp_battery_csv_data("raw_data/lamp_battery/csv/서울특별시_송파구_폐형광등 폐건전지 분리수거함 위치_20200615.csv", "parsed_csv/lamp_battery/songpa.csv", "parsed_csv/lamp_battery/missed.csv")
