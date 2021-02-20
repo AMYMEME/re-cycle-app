4e52daab59a3961002384ecc1cd3d391f32842f5
amy choi <cgc8016@ewhain.net>
2021-02-21 00:04:42 +0900

    feat(dongdaemun_lamp_battery)

--- data/main.py
+++ data/main.py
@@ -1,7 +1,7 @@
 from add_data_firestore.recycle_guide import add_csv_data
-from kakao_map_location import gangnam
+from kakao_map_location import dongdaemun
 
 # add_csv_data("parsed_csv/recycle-guide.csv")
 # get_cloth_csv_data("raw_data/cloth/csv/서울특별시 구로구_의류수거함_20200930.csv", "parsed_csv/cloth/guro.csv", "parsed_csv/cloth/missed.csv")
 # dongjak.get_lamp_battery_csv_data("raw_data/lamp_battery/csv/서울특별시 동작구 폐형광등(폐건전지) 분리수거함 현황_20200820.csv", "parsed_csv/lamp_battery/dongjak.csv", "parsed_csv/lamp_battery/missed.csv")
-gangnam.get_lamp_battery_csv_data("raw_data/lamp_battery/csv/서울특별시_강남구_폐형광등 폐건전지 분리수거함 위치_20200827.csv", "parsed_csv/lamp_battery/gangnam.csv", "parsed_csv/lamp_battery/missed.csv")
+dongdaemun.get_lamp_battery_csv_data("raw_data/lamp_battery/csv/서울특별시_동대문구_폐건전지폐형광등분리수거함_20200811.csv", "parsed_csv/lamp_battery/dongdaemun.csv", "parsed_csv/lamp_battery/missed.csv")
