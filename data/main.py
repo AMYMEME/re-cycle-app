e37020f7cf344946fb3622e943c7e87e784f0344
amy choi <cgc8016@ewhain.net>
2021-02-22 02:16:53 +0900

    feat(seocho_cloth)

--- data/main.py
+++ data/main.py
@@ -1,7 +1,7 @@
 from add_data_firestore.recycle_guide import add_csv_data
-from kakao_map_location import mapo
+from kakao_map_location import seocho
 
 # add_csv_data("parsed_csv/recycle-guide.csv")
 # get_cloth_csv_data("raw_data/cloth/csv/서울특별시 구로구_의류수거함_20200930.csv", "parsed_csv/cloth/guro.csv", "parsed_csv/cloth/missed.csv")
 # dongjak.get_lamp_battery_csv_data("raw_data/lamp_battery/csv/서울특별시 동작구 폐형광등(폐건전지) 분리수거함 현황_20200820.csv", "parsed_csv/lamp_battery/dongjak.csv", "parsed_csv/lamp_battery/missed.csv")
-mapo.get_cloth_csv_data("raw_data/cloth/xls/마포구_의류수거함.csv", "parsed_csv/cloth/mapo.csv", "parsed_csv/cloth/missed.csv")
+seocho.get_cloth_csv_data("raw_data/cloth/xls/서울특별시 서초구_의류수거함_소재지_현황.csv", "parsed_csv/cloth/seocho.csv", "parsed_csv/cloth/missed.csv")
