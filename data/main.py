9f435e5f1757f396d61403e738cad03a24fa9474
amy choi <cgc8016@ewhain.net>
2021-02-24 16:25:21 +0900

    feat(mapo_lamp_battery)

--- data/main.py
+++ data/main.py
@@ -1,7 +1,7 @@
 from add_data_firestore.recycle_guide import add_csv_data
-from kakao_map_location import gangbuk
+from kakao_map_location import mapo
 
 # add_csv_data("parsed_csv/recycle-guide.csv")
 # get_cloth_csv_data("raw_data/cloth/csv/서울특별시 구로구_의류수거함_20200930.csv", "parsed_csv/cloth/guro.csv", "parsed_csv/cloth/missed.csv")
 # dongjak.get_lamp_battery_csv_data("raw_data/lamp_battery/csv/서울특별시 동작구 폐형광등(폐건전지) 분리수거함 현황_20200820.csv", "parsed_csv/lamp_battery/dongjak.csv", "parsed_csv/lamp_battery/missed.csv")
-gangbuk.get_lamp_battery_csv_data("raw_data/lamp_battery/xls/강북구_폐형광등_폐건전지.csv", "parsed_csv/lamp_battery/gangbuk.csv", "parsed_csv/lamp_battery/missed.csv")
+mapo.get_lamp_battery_csv_data("raw_data/lamp_battery/xls/마포구_폐형광등_폐건전지.csv", "parsed_csv/lamp_battery/mapo.csv", "parsed_csv/lamp_battery/missed.csv")
